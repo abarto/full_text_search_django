bf323f1b722fb3a797ab4ac860a373cf2fbf5850
Agustin Barto <abarto@gmail.com>
2016-01-05 15:37:40 -0300

    Add __str__ to models

--- full_text_search_django/items/models.py
+++ full_text_search_django/items/models.py
@@ -5,7 +5,13 @@ class Item(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(max_length=2000)
 
+    def __str__(self):
+        return self.name
+
 
 class Part(models.Model):
     item = models.ForeignKey('items.Item', related_name='parts')
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
