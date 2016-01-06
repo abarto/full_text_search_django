08e8b41c70ea46f78019c932320ce78cb973104c
Agustin Barto <abarto@gmail.com>
2016-01-06 11:23:03 -0300

    Add Item and Part querysets

--- full_text_search_django/items/models.py
+++ full_text_search_django/items/models.py
@@ -1,9 +1,45 @@
 from django.db import models
 
 
+class ItemQueryset(models.QuerySet):
+    def text_search_name(self, name):
+        return self.extra(
+            select={'rank': "MATCH (name) AGAINST (%s IN NATURAL LANGUAGE MODE)"},
+            select_params=(name,),
+            where=('MATCH (name) AGAINST (%s IN NATURAL LANGUAGE MODE) > 0',),
+            params=(name,),
+            order_by=('-rank',)
+        )
+
+    def text_search_part_name(self, name):
+        return self.filter(id__in=Part.objects.text_search_name(name))
+
+
+class PartQueryset(models.QuerySet):
+    def text_search_name(self, name):
+        return self.extra(
+            select={'rank': "MATCH (name) AGAINST (%s IN NATURAL LANGUAGE MODE)"},
+            select_params=(name,),
+            where=('MATCH (name) AGAINST (%s IN NATURAL LANGUAGE MODE) > 0',),
+            params=(name,),
+            order_by=('-rank',)
+        )
+
+    def text_search_item_name(self, name):
+        return self.select_related().extra(
+            select={'rank': "MATCH (items_item.name) AGAINST (%s IN NATURAL LANGUAGE MODE)"},
+            select_params=(name,),
+            where=('MATCH (items_item.name) AGAINST (%s IN NATURAL LANGUAGE MODE) > 0',),
+            params=(name,),
+            order_by=('-rank',)
+        )
+
+
 class Item(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(max_length=2000)
+
+    objects = ItemQueryset.as_manager()
 
     def __str__(self):
         return self.name
@@ -12,6 +48,8 @@ class Item(models.Model):
 class Part(models.Model):
     item = models.ForeignKey('items.Item', related_name='parts')
     name = models.CharField(max_length=200)
+
+    objects = PartQueryset.as_manager()
 
     def __str__(self):
         return self.name
