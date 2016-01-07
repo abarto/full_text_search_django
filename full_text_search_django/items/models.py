ec22e3ea514595cc908cb72979e20936cf1b08a7
Agustin Barto <abarto@gmail.com>
2016-01-07 09:47:53 -0300

    Fix part name search

--- full_text_search_django/items/models.py
+++ full_text_search_django/items/models.py
@@ -12,7 +12,7 @@ class ItemQueryset(models.QuerySet):
         )
 
     def text_search_part_name(self, name):
-        return self.filter(id__in=Part.objects.text_search_name(name))
+        return self.filter(parts__id__in=Part.objects.text_search_name(name))
 
 
 class PartQueryset(models.QuerySet):
